7844696ac6943533478efe666550c7b497eeeaf0
ExeVirus <ddsallad@gmail.com>
2021-12-11 14:02:52 -0500

    Namespace everything

--- init.lua
+++ init.lua
@@ -1,56 +1,55 @@
-local lady = {}
+local lady_assets = {}
 
-lady.register_mesh = function(name)
+lady_assets.register_mesh = function(name)
 	autobox.register_node(
 		"lady_assets:"..name, --node name
-		name..".box", --node bounding box
+		"lady_assets_"..name..".box", --node bounding box
 		{ --node def
 			description =  "lady_assets:"..name,
 			drawtype = "mesh",
-			mesh = name..".obj",
+			mesh = "lady_assets_"..name..".obj",
 			sunlight_propagates = true,
 			paramtype2 = "facedir",
 			paramtype = "light",
-			tiles = {name..".png"},
+			tiles = {"lady_assets_"..name..".png"},
 			groups = { oddly_breakable_by_hand=2 },
 		},
 		true
 	)
 end
 
-lady.register_mesh("bell_flower")
-lady.register_mesh("carrot")
-lady.register_mesh("daffodil")
-lady.register_mesh("desert_flower")
-lady.register_mesh("desert_marigold")
-lady.register_mesh("fence")
-lady.register_mesh("fern")
-lady.register_mesh("flower_pot")
-lady.register_mesh("garden_rake")
-lady.register_mesh("grass")
-lady.register_mesh("grass_blade_1")
-lady.register_mesh("grass_medium")
-lady.register_mesh("grass_plant_1")
-lady.register_mesh("grass_short")
-lady.register_mesh("grass_tuft")
-lady.register_mesh("hummingbird")
-lady.register_mesh("ivy")
-lady.register_mesh("leafy_green")
-lady.register_mesh("mushroom")
-lady.register_mesh("mushroom_2")
-lady.register_mesh("pastel_flowers")
-lady.register_mesh("pushlin")
-lady.register_mesh("roses")
-lady.register_mesh("slug_1")
-lady.register_mesh("slug_2")
-lady.register_mesh("star")
-lady.register_mesh("tree_roots")
-lady.register_mesh("tree_stump")
-lady.register_mesh("trowel")
-lady.register_mesh("tulips")
-lady.register_mesh("twig_1")
-lady.register_mesh("vine_1")
-lady.register_mesh("watering_can")
+lady_assets.register_mesh("bell_flower")
+lady_assets.register_mesh("carrot")
+lady_assets.register_mesh("daffodil")
+lady_assets.register_mesh("desert_flower")
+lady_assets.register_mesh("desert_marigold")
+lady_assets.register_mesh("fence")
+lady_assets.register_mesh("fern")
+lady_assets.register_mesh("flower_pot")
+lady_assets.register_mesh("garden_rake")
+lady_assets.register_mesh("grass")
+lady_assets.register_mesh("grass_blade_1")
+lady_assets.register_mesh("grass_medium")
+lady_assets.register_mesh("grass_plant_1")
+lady_assets.register_mesh("grass_short")
+lady_assets.register_mesh("grass_tuft")
+lady_assets.register_mesh("hummingbird")
+lady_assets.register_mesh("ivy")
+lady_assets.register_mesh("leafy_green")
+lady_assets.register_mesh("mushroom")
+lady_assets.register_mesh("mushroom_2")
+lady_assets.register_mesh("pastel_flowers")
+lady_assets.register_mesh("pushlin")
+lady_assets.register_mesh("roses")
+lady_assets.register_mesh("slug_1")
+lady_assets.register_mesh("slug_2")
+lady_assets.register_mesh("tree_roots")
+lady_assets.register_mesh("tree_stump")
+lady_assets.register_mesh("trowel")
+lady_assets.register_mesh("tulips")
+lady_assets.register_mesh("twig_1")
+lady_assets.register_mesh("vine_1")
+lady_assets.register_mesh("watering_can")
 
 --Water For game
 minetest.register_node("lady_assets:water", {
@@ -62,12 +61,12 @@ minetest.register_node("lady_assets:water", {
 			{-0.5, -0.5, -0.5, 0.5, 0.475, 0.5},
 		},
 	},
-	connects_to = "lady_assets:water",
+	connects_to = "lady_assets:lady_assets_water",
 	connect_sides = { "top", "bottom"},
 	waving = 3,
 	tiles = {
 		{
-			name = "water.png",
+			name = "lady_assets_water.png",
 			backface_culling = true,
 			animation = {
 				type = "vertical_frames",
@@ -86,7 +85,7 @@ minetest.register_node("lady_assets:water", {
 })
 
 --Water For game
-minetest.register_node("lady_assets:_star", {
+minetest.register_node("lady_assets:star", {
 	description = "Star",
 	drawtype = "mesh",
 	collision_box = {
@@ -96,10 +95,10 @@ minetest.register_node("lady_assets:_star", {
 		},
 	},
 	use_texture_alpha = true,
-	mesh = "_star.obj",
+	mesh = "lady_assets_star.obj",
 	tiles = {
 		{
-			name = "_star.png",
+			name = "lady_assets_star.png",
 			animation = {
 				type = "vertical_frames",
 				aspect_w = 4,
@@ -118,7 +117,7 @@ minetest.register_node("lady_assets:_star", {
 minetest.register_node("lady_assets:grass", {
 	description = "grass",
 	drawtype = "normal",
-	tiles = {"grass.png"},
+	tiles = {"lady_assets_grass.png"},
 	paramtype = "light",
 	paramtype2 = "facedir",
 	groups = { oddly_breakable_by_hand = 2},
@@ -138,7 +137,7 @@ minetest.register_globalstep(function(dtime)
 				minetest.chat_send_player("singleplayer", "Drowning!")
 			else
 				_node = minetest.get_node(_pos)
-				if _node.name == "lady_assets:_star" then
+				if _node.name == "lady_assets:star" then
 					minetest.chat_send_player("singleplayer", "star")
 				end
 			end
@@ -148,6 +147,6 @@ end)
 
 stairsplus:register_all("lady_assets", "grass", "lady_assets:grass", {
 	description = "Grass",
-	tiles = {"grass.png"},
+	tiles = {"lady_assets_grass.png"},
 	groups = {oddly_breakable_by_hand=2},
 })
